65d682f1ec8b61ee93e5729cd344dae35cee7ab1
09Uno <bruno.a.oliveira.1209@gmail.com>
2023-01-23 14:29:43 -0300

    home, away, time

--- src/gamesInfoReq/allGames/nextGamesController.ts
+++ src/gamesInfoReq/allGames/nextGamesController.ts
@@ -1,23 +1,41 @@
 import { NextGamesService } from "./nextGamesService";
 import { Request, Response } from "express";
 
-class NextGamesController{
 
 
-    async handle(req: Request, res: Response){
 
 
-        const nextGamesService =new NextGamesService();
-
-        const games = await nextGamesService.execute()
+class NextGamesController {
 
 
-        return res.json(games)
+    async handle(req: Request, res: Response) {
+
+
+
+        const nextGamesService = new NextGamesService();
+
+        const games = await nextGamesService.execute();
+
+        let i = 0;
+        var data1 = [];
+        while (i < games.length) {
+            data1.push(games.slice(i, i + 3));
+            i += 3;
+        }
+
+        const data = data1;
+
+        return res.json(data);
+
+
 
     }
 
 
 
+
 }
 
-export {NextGamesController}
+
+
+export { NextGamesController }
--- src/gamesInfoReq/allGames/nextGamesService.ts
+++ src/gamesInfoReq/allGames/nextGamesService.ts
@@ -1,44 +1,107 @@
-import { json } from "express";
 import puppeteer from "puppeteer";
 
+interface game {
 
-
-class NextGamesService {
-
-
-
-    async execute() {
-
-        const browser = await puppeteer.launch({ headless: false });
-        const page = await browser.newPage();
-        await page.goto('https://www.flashscore.com.br');
-
-        const games = await page.$$('#live-table > section > div > div');
-
-        const game1 = []
-
-        for (const game of games) {
-
-
-            const time = await page.evaluate((el) => el.querySelector('#g_1_IcYs8jIk > div.event__time')?.textContent, game)
-
-            const home = await page.evaluate((el) => el.querySelector('div.event__participant.event__participant--home')?.textContent, game)
-
-            const away = await page.evaluate((el) => el.querySelector('div.event__participant.event__participant--away')?.textContent, game)
-
-            const league = await page.evaluate((el) => el.querySelector('div.icon--flag.event__title.fl_81 > div > span.event__title--name')?.textContent, game)
-
-
-            game1.push({ time, home, away, league});
-
-        }
-        return ({game1})
-
-    }
+    country: string | null | undefined;
+    league: string | null | undefined
+    home: string | null | undefined;
+    away: string | null | undefined;
+    time: string | null | undefined;
+    status: string | null | undefined;
 
 }
 
+class NextGamesService {
+    async execute() {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.goto("https://www.flashscore.com.br");
+
+        const element = await page.$("#live-table > section");
+
+        const section = await element?.$$(".event .soccer .event__match--twoLine");
+        const section2 = await element?.$$(".event__header.top");
+
+        const gamesList = [];
+
+
+        if (section != null && section2 != null) {
+            const game: game = {
+
+                country: '',
+                league: '',
+                home: '',
+                away: '',
+                time: '',
+                status: ''
+
+            };
+
+            for (let index = 0; index < section.length; index++) {
+                const element = section[index];
+                const element2 = section2[index];
+
+                const dataCountry = await element2?.$('div.event__header .event__title--type');
+                const propsCountry = await dataCountry?.getProperty("textContent");
+
+                const dataLeague = await element2?.$('div.icon--flag.event__title.fl_3 > div > span.event__title--name');
+                const propsLeague = await dataLeague?.getProperty("textContent");
+
+                const dataTime = await element?.$('div.event__time');
+                const propsTime = await dataTime?.getProperty("textContent");
+
+                const dataStatus = await element?.$('div.event__stage > div');
+                const propStatus = await dataStatus?.getProperty("textContent");
+
+
+                const dataHome = await element?.$("div.event__participant.event__participant--home");
+                const propsHome = await dataHome?.getProperty("textContent");
+
+                const dataAway = await element?.$("div.event__participant.event__participant--away");
+                const propsAway = await dataAway?.getProperty("textContent");
+
+
+                game.country = await propsCountry?.jsonValue();
+                game.league = await propsLeague?.jsonValue();
+
+                game.time = await propsTime?.jsonValue();
+                game.home = await propsHome?.jsonValue();
+                game.away = await propsAway?.jsonValue();
+                game.status = await propStatus?.jsonValue();
 
 
 
-export { NextGamesService }
+
+                // gamesList.push(game.country);
+
+                // gamesList.push(game.league);
+
+                if (game.home) {
+                    gamesList.push(game.home);
+
+                }
+
+                if (game.away) {
+                    gamesList.push(game.away);
+
+                }
+                if (game.time || game.status) {
+                    gamesList.push(game.status || game.time);
+
+                }
+
+
+
+
+
+            }
+        }
+
+
+
+        console.log(gamesList);
+        return gamesList;
+    }
+}
+
+export { NextGamesService };
